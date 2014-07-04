ea6c0969b08b3a7b8f98f7135d0b38345808d590
Gael de Chalendar <gael.de-chalendar@cea.fr>
2014-07-04 17:37:05 +0200

    Add new build system to build all Lima in one shot
    A new cmake project at root delegates the build of subprojects as
    external ones. A bash script handles all boring details.

--- gbuild.sh
+++ gbuild.sh
@@ -15,20 +15,54 @@
 #   along with LIMA.  If not, see <http://www.gnu.org/licenses/>
 #!/bin/bash
 
-usage() { echo "Usage: $0 [-m <debug|release>] [-v <val|rev>]" 1>&2; exit 1; }
+usage() 
+{ 
+cat << EOF 1>&2; exit 1; 
+Synopsis: $0 [OPTIONS]
+
+Options default values are in parentheses.
+
+  -m mode       <(debug)|release> compile mode
+  -p boolean    <(true)|false> will build in parallel (make -jn) if true. 
+                Necessary to be able to build with no parallelism as  it currently fail on 
+                some machines.
+  -r resources  <precompiled|(build)> build the linguistic resources or use the
+                precompiled ones
+  -t buildtype  <text|textse|(all)> either build only linguistic analysis modules
+                (text), text only search engine (textse) or all amose (all)
+  -v version    <(val)|rev> version number is set either to the value set by  
+                config files or to the short git sha1
+EOF
+exit 1
+}
 
 mode="debug"
-release="1"
+version="val"
+buildtype="all"
+resources="build"
+parallel="true"
 
-while getopts ":m:v:" o; do
+while getopts ":m:p:r:t:v:" o; do
     case "${o}" in
         m)
             mode=${OPTARG}
-            ((mode == "debug" || mode == "release")) || usage
+            [[ "$mode" == "debug" || "$mode" == "release" ]] || usage
+            ;;
+        p)
+            parallel=${OPTARG}
+            [[ "$parallel" == "true" || "$parallel" == "false" ]] || usage
+            ;;
+        r)
+            resources=${OPTARG}
+            [[ "$resources" == "precompiled" || "$resources" == "build" ]] || usage
+            ;;
+        t)
+            buildtype=${OPTARG}
+            [[ "$buildtype" == "text" || "$buildtype" == "textse" || "$buildtype" == "all" ]] || usage
             ;;
         v)
-            release=${OPTARG}
-            ((release == "val" || release == "rev")) || usage
+            version=$OPTARG
+            [[ "$version" == "val" ||  "$version" == "rev" ]] || usage
             ;;
         *)
             usage
@@ -44,24 +78,34 @@ current_project=`basename $PWD`
 build_prefix=$AMOSE_BUILD_DIR/$current_branch
 source_dir=$PWD
 
-if [[ $release = "rev" ]]; then
+if [[ $version = "rev" ]]; then
 release="$current_timestamp-$current_revision"
+else
+release="1"
+fi
+
+if [[ $parallel = "true" ]]; then
+j=`grep -c ^processor /proc/cpuinfo`
+echo "Parallel build on $j processors"
+else
+echo "Linear build"
+j="1"
 fi
 
 if [[ $mode == "release" ]]; then
-echo "version='$release'"
+echo "version='$version'"
     install -d $build_prefix/release/$current_project
     pushd $build_prefix/release/$current_project
-    cmake -DAMOSE_VERSION_RELEASE="$release" -DCMAKE_INSTALL_PREFIX=$AMOSE_DIST -DCMAKE_BUILD_TYPE=Release $source_dir
-    make -j4 && make install && make package
+    cmake -DAMOSE_RESOURCES="$resources" -DAMOSE_BUILD_TYPE="$buildtype" -DAMOSE_VERSION_RELEASE="$release" -DCMAKE_INSTALL_PREFIX=$AMOSE_DIST -DCMAKE_BUILD_TYPE=Release $source_dir
+    make -j$j && make test && make install && make package
     result=$?
     popd
 else
     # default is : compile in debug mode in debug/ directory
     install -d $build_prefix/debug/$current_project
     pushd $build_prefix/debug/$current_project
-    cmake -DAMOSE_VERSION_RELEASE=$release -DCMAKE_INSTALL_PREFIX=$AMOSE_DIST -DCMAKE_BUILD_TYPE=Debug $source_dir
-    make -j`grep -c ^processor /proc/cpuinfo` && make install
+    cmake -DAMOSE_RESOURCES="$resources" -DAMOSE_BUILD_TYPE="$buildtype" -DAMOSE_VERSION_RELEASE=$release -DCMAKE_INSTALL_PREFIX=$AMOSE_DIST -DCMAKE_BUILD_TYPE=Debug $source_dir
+    make -j$j && make test && make install
     result=$?
     popd
 fi
