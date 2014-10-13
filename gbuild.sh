ed221d8f984a1e4abb4a6ad65b246db42a3b4a60
Jean-Noël Lesdema <jean-noel.lesdema@cea.fr>
2014-10-13 20:31:05 +0200

    fix exit code in case 'set -o errexit' is used by caller scripts

--- gbuild.sh
+++ gbuild.sh
@@ -100,7 +100,13 @@ install -d $build_prefix/$mode/$current_project
 pushd $build_prefix/$mode/$current_project
 cmake -DCMAKE_BUILD_TYPE:STRING=$cmake_mode -DLIMA_RESOURCES:PATH="$resources" -DLIMA_VERSION_RELEASE:STRING="$release" -DCMAKE_INSTALL_PREFIX:PATH=$LIMA_DIST $source_dir
 
-make -j$j && [ "x$current_project_name" != "xproject(Lima)" ] && make test && make install
+make -j$j 
+
+if [ "x$current_project_name" != "xproject(Lima)" ];
+then
+  make test && make install
+fi
+
 result=$?
 popd
 
