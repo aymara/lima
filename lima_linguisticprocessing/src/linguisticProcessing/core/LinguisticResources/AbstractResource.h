de3c53a6be43b7016abde0f2d02168f210907831
Jean-Noël Lesdema <jean-noel.lesdema@cea.fr>
2015-04-16 15:16:47 +0200

    fix: link to lima-tools

--- lima_linguisticprocessing/src/linguisticProcessing/core/LinguisticResources/AbstractResource.h
+++ lima_linguisticprocessing/src/linguisticProcessing/core/LinguisticResources/AbstractResource.h
@@ -21,6 +21,9 @@
 #ifndef LIMA_LINGUISTICPROCESSING_LINGUISTICRESOURCES_ABSTRACTRESOURCES_H
 #define LIMA_LINGUISTICPROCESSING_LINGUISTICRESOURCES_ABSTRACTRESOURCES_H
 
+
+#include "LinguisticResourcesExport.h"
+
 #include "linguisticProcessing/LinguisticProcessingCommon.h"
 
 #include "common/XMLConfigurationFiles/groupConfigurationStructure.h"
@@ -36,7 +39,7 @@ struct ResourceInitializationParameters {
   MediaId language;
 };
 
-class AbstractResourcePrivate;
+class  AbstractResourcePrivate;
 /** 
   * @brief resource abstraction. All resource should inherit from this class
   * @author Benoit Mathieu <mathieub@zoe.cea.fr>
@@ -48,7 +51,7 @@ class AbstractResourcePrivate;
   * @ref resourceFileChanged signal to do whatever necessary with this changed resource, for 
   * example reloading it.
   */
-class AbstractResource : public QObject, public InitializableObject<AbstractResource,ResourceInitializationParameters>
+class LIMA_LINGUISTICRESOURCES_EXPORT AbstractResource : public QObject, public InitializableObject<AbstractResource,ResourceInitializationParameters>
 {
   Q_OBJECT
 public:
