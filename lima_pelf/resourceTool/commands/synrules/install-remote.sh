efa7269e32a00fc86968a5bc4db93d1ab1d0747b
Jean-Noël Lesdema <jean-noel.lesdema@cea.fr>
2015-04-30 09:58:29 +0200

    replace r2l occs for fre

--- lima_pelf/resourceTool/commands/synrules/install-remote.sh
+++ lima_pelf/resourceTool/commands/synrules/install-remote.sh
@@ -9,14 +9,14 @@ SYNRULES_PATH=$RESOURCE_PATH/syntacticAnalysis
 REMOTE_SYNRULES_PATH=/home/leffe/Lima/Sources/linguisticData/syntacticAnalysis
 
 echo "Syncing source files"
-rsyncexec $SYNRULES_PATH/rules-l2r-fre-homodeps.txt leffe@calamondin:$REMOTE_SYNRULES_PATH/rules-l2r-fre-homodeps.txt
-rsyncexec $SYNRULES_PATH/rules-r2l-fre-homodeps.txt leffe@calamondin:$REMOTE_SYNRULES_PATH/rules-r2l-fre-homodeps.txt
+rsyncexec $SYNRULES_PATH/rules-fre-homodeps-pass1.txt leffe@calamondin:$REMOTE_SYNRULES_PATH/rules-fre-homodeps-pass1.txt
+rsyncexec $SYNRULES_PATH/rules-fre-homodeps-pass2.txt leffe@calamondin:$REMOTE_SYNRULES_PATH/rules-fre-homodeps-pass2.txt
 rsyncexec $SYNRULES_PATH/rules-fre-heterodeps.txt leffe@calamondin:$REMOTE_SYNRULES_PATH/rules-fre-heterodeps.txt
-rsyncexec $SYNRULES_PATH/rules-r2l-pass1-fre-homodeps.txt leffe@calamondin:$REMOTE_SYNRULES_PATH/rules-r2l-pass1-fre-homodeps.txt
+rsyncexec $SYNRULES_PATH/rules-fre-homodeps-pass0.txt leffe@calamondin:$REMOTE_SYNRULES_PATH/rules-fre-homodeps-pass0.txt
 rsyncexec $SYNRULES_PATH/simplification-first-rules-fre.txt leffe@calamondin:$REMOTE_SYNRULES_PATH/simplification-first-rules-fre.txt
 rsyncexec $SYNRULES_PATH/simplification-rules-fre.txt leffe@calamondin:$REMOTE_SYNRULES_PATH/simplification-rules-fre.txt
 rsyncexec $SYNRULES_PATH/simplification-last-rules-fre.txt leffe@calamondin:$REMOTE_SYNRULES_PATH/simplification-last-rules-fre.txt
-rsyncexec $SYNRULES_PATH/rules-l2r-fre-pleonasticPronouns.txt leffe@calamondin:$REMOTE_SYNRULES_PATH/rules-l2r-fre-pleonasticPronouns.txt
+rsyncexec $SYNRULES_PATH/rules-fre-pleonasticPronouns.txt leffe@calamondin:$REMOTE_SYNRULES_PATH/rules-fre-pleonasticPronouns.txt
 rsyncexec $SYNRULES_PATH/categoriesClassesDeclaration-fre.txt leffe@calamondin:$REMOTE_SYNRULES_PATH/categoriesClassesDeclaration-fre.txt
 rsyncexec $SYNRULES_PATH/rules-fre-heterodeps-specializer.txt leffe@calamondin:$REMOTE_SYNRULES_PATH/rules-fre-heterodeps-specializer.txt
 rsyncexec $SYNRULES_PATH/rules-l2r-fre-dummy.txt leffe@calamondin:$REMOTE_SYNRULES_PATH/rules-l2r-fre-dummy.txt
