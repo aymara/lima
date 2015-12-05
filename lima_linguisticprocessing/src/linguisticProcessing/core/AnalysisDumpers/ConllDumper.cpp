b227d31ae314a4a8ea5018859985c64d42c446ec
Gael de Chalendar <gael.de-chalendar@cea.fr>
2015-12-05 13:41:44 +0100

    Add a named entity type column to the CONLL output

--- lima_linguisticprocessing/src/linguisticProcessing/core/AnalysisDumpers/ConllDumper.cpp
+++ lima_linguisticprocessing/src/linguisticProcessing/core/AnalysisDumpers/ConllDumper.cpp
@@ -361,7 +361,7 @@ LimaStatusCode ConllDumper::process(AnalysisContent& analysis) const
       {
         const QString macro=QString::fromUtf8(static_cast<const Common::MediaticData::LanguageData&>(Common::MediaticData::MediaticData::single().mediaData(m_d->m_language)).getPropertyCodeManager().getPropertyManager("MACRO").getPropertySymbolicValue(morphoData->firstValue(*m_d->m_propertyAccessor)).c_str());
         const QString micro=QString::fromUtf8(static_cast<const Common::MediaticData::LanguageData&>(Common::MediaticData::MediaticData::single().mediaData(m_d->m_language)).getPropertyCodeManager().getPropertyManager("MICRO").getPropertySymbolicValue(morphoData->firstValue(*m_d->m_propertyAccessor)).c_str());
-      LDEBUG << "ConllDumper::process graphTag:" << micro;
+        LDEBUG << "ConllDumper::process graphTag:" << micro;
 
         std::string inflectedToken=ft->stringForm().toUtf8().constData();
         std::string lemmatizedToken;
@@ -370,7 +370,29 @@ LimaStatusCode ConllDumper::process(AnalysisContent& analysis) const
           lemmatizedToken=sp[(*morphoData)[0].lemma].toUtf8().constData();
         }
 
-        QString conllRelName = "-";
+        QString neType = QString::fromUtf8("_") ;
+        std::set< AnnotationGraphVertex > anaVertices = annotationData->matches("PosGraph",v,"AnalysisGraph");
+        // note: anaVertices size should be 0 or 1
+        for (std::set< AnnotationGraphVertex >::const_iterator anaVerticesIt = anaVertices.begin();
+            anaVerticesIt != anaVertices.end(); anaVerticesIt++)
+        {
+          std::set< AnnotationGraphVertex > matches = annotationData->matches("AnalysisGraph",*anaVerticesIt,"annot");
+          for (std::set< AnnotationGraphVertex >::const_iterator it = matches.begin();
+              it != matches.end(); it++)
+          {
+            AnnotationGraphVertex vx=*it;
+            if (annotationData->hasAnnotation(vx, Common::Misc::utf8stdstring2limastring("SpecificEntity")))
+            {
+              const SpecificEntityAnnotation* se =
+                annotationData->annotation(vx, Common::Misc::utf8stdstring2limastring("SpecificEntity")).
+                pointerValue<SpecificEntityAnnotation>();
+              neType = Common::MediaticData::MediaticData::single().getEntityName(se->getType());
+              break;
+            }
+          }
+          if (neType != "_") break;
+        }
+        QString conllRelName = "_";
         int targetConllId = 0;
         if (vertexDependencyInformations.count(v)!=0)
         {
@@ -391,27 +413,29 @@ LimaStatusCode ConllDumper::process(AnalysisContent& analysis) const
 //             LERROR << "ConllDumper::process" << relName << "not found in mapping";
           }
         }
-        // CONLL-X format
+        // Modified CONLL-X format with an extra named entity type column
         // http://ilk.uvt.nl/conll/#dataformat
         // 1   ID  Token counter, starting at 1 for each new sentence.
         // 2   FORM  Word form or punctuation symbol.
         // 3   LEMMA   Lemma or stem (depending on particular data set) of word form, or an underscore if not available.
         // 4   CPOSTAG   Coarse-grained part-of-speech tag, where tagset depends on the language.
         // 5   POSTAG  Fine-grained part-of-speech tag, where the tagset depends on the language, or identical to the coarse-grained part-of-speech tag if not available.
-        // 6   FEATS   Unordered set of syntactic and/or morphological features (depending on the particular language), separated by a vertical bar (|), or an underscore if not available.
-        // 7   HEAD  Head of the current token, which is either a value of ID or zero ('0'). Note that depending on the original treebank annotation, there may be multiple tokens with an ID of zero.
-        // 8   DEPREL  Dependency relation to the HEAD. The set of dependency relations depends on the particular language. Note that depending on the original treebank annotation, the dependency relation may be meaningfull or simply 'ROOT'.
-        // 9   PHEAD   Projective head of current token, which is either a value of ID or zero ('0'), or an underscore if not available. Note that depending on the original treebank annotation, there may be multiple tokens an with ID of zero. The dependency structure resulting from the PHEAD column is guaranteed to be projective (but is not available for all languages), whereas the structures resulting from the HEAD column will be non-projective for some sentences of some languages (but is always available).
-        // 10  PDEPREL   Dependency relation to the PHEAD, or an underscore if not available. The set of dependency relations depends on the particular language. Note that depending on the original treebank annotation, the dependency relation may be meaningfull or simply 'ROOT'.
+        // 6   NER Extra column: Named entity type
+        // 7   FEATS   Unordered set of syntactic and/or morphological features (depending on the particular language), separated by a vertical bar (|), or an underscore if not available.
+        // 8   HEAD  Head of the current token, which is either a value of ID or zero ('0'). Note that depending on the original treebank annotation, there may be multiple tokens with an ID of zero.
+        // 9   DEPREL  Dependency relation to the HEAD. The set of dependency relations depends on the particular language. Note that depending on the original treebank annotation, the dependency relation may be meaningfull or simply 'ROOT'.
+        // 10  PHEAD   Projective head of current token, which is either a value of ID or zero ('0'), or an underscore if not available. Note that depending on the original treebank annotation, there may be multiple tokens an with ID of zero. The dependency structure resulting from the PHEAD column is guaranteed to be projective (but is not available for all languages), whereas the structures resulting from the HEAD column will be non-projective for some sentences of some languages (but is always available).
+        // 11  PDEPREL   Dependency relation to the PHEAD, or an underscore if not available. The set of dependency relations depends on the particular language. Note that depending on the original treebank annotation, the dependency relation may be meaningfull or simply 'ROOT'.
 
         QString targetConllIdString = targetConllId > 0 ? QString("%1").arg(targetConllId) : "_";
         dstream->out()  << tokenId 
                         << "\t" << inflectedToken 
                         << "\t" << lemmatizedToken 
-                        << "\t" << macro  
-                        << "\t" << micro 
+                        << "\t" << macro.toUtf8().constData()
+                        << "\t" << micro.toUtf8().constData() 
+                        << "\t" << neType.toUtf8().constData()
                         << "\t" << "_" 
-                        << "\t" << targetConllIdString 
+                        << "\t" << targetConllIdString.toUtf8().constData() 
                         << "\t" << conllRelName.toUtf8().constData() 
                         << "\t" << "_"
                         << "\t" << "_";
