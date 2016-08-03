94db20f4b585a9e91981b536179aa7e68dbfed63
simon <simon>
2016-08-03 09:12:46 +0200

    Improve find QhttpServer for Windows

--- lima_linguisticprocessing/cmake/Modules/FindQHttpServer.cmake
+++ lima_linguisticprocessing/cmake/Modules/FindQHttpServer.cmake
@@ -6,12 +6,10 @@
 # - Find the native QHttpServer includes and library
 #
 # This module defines
-#  QHTTPSERVERCPP_INCLUDE_DIR, where to find soci.h, etc.
-#  QHTTPSERVERCPP_LIBRARY_DIRS, the libraries to link against to use QHttpServer.
-#  QHTTPSERVER_DEFINITIONS - You should add_definitons(${QHTTPSERVER_DEFINITIONS}) before compiling code that includes png library files.
+#  QHTTPSERVER_INCLUDE_DIR, where to find qhttpserver.h, etc.
+#  QHTTPSERVER_LIBRARY_LIB, the libraries to link against to use QHttpServer.
 #  QHTTPSERVER_FOUND, If false, do not try to use QHttpServer.
 # also defined, but not for general use are
-#  QHTTPSERVERCPP_LIBRARY_DIRS, where to find the QHttpServer library.
 
 #=============================================================================
 # Copyright 2002-2009 Kitware, Inc.
@@ -26,20 +24,18 @@
 # (To distributed this file outside of CMake, substitute the full
 #  License text for the above reference.)
 
-
-find_path(QHTTPSERVERCPP_INCLUDE_DIR qhttpserver.h
+find_path(QHTTPSERVER_INCLUDE_DIR qhttpserver.h
   PATHS $ENV{LIMA_EXTERNALS}/include
 )
 
-set(QHTTPSERVER_NAMES ${QHTTPSERVER_NAMES} qhttpserver)
-find_library(QHTTPSERVERCPP_LIBRARY_DIRS NAMES ${QHTTPSERVER_NAMES}
+list(APPEND QHTTPSERVER_NAMES qhttpserver libqhttpserver)
+find_library(QHTTPSERVER_LIBRARY_LIB NAMES ${QHTTPSERVER_NAMES}
   PATHS $ENV{LIMA_EXTERNALS}/lib 
 )
 
 # handle the QUIETLY and REQUIRED arguments and set QHTTPSERVER_FOUND to TRUE if
 # all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(QHTTPSERVER  DEFAULT_MSG  QHTTPSERVERCPP_LIBRARY_DIRS QHTTPSERVERCPP_INCLUDE_DIR)
+find_package_handle_standard_args(QHTTPSERVER QHTTPSERVER_INCLUDE_DIR QHTTPSERVER_LIBRARY_LIB)
 
-mark_as_advanced(QHTTPSERVERCPP_INCLUDE_DIR QHTTPSERVERCPP_LIBRARY_DIRS )
-
+mark_as_advanced(QHTTPSERVER_INCLUDE_DIR QHTTPSERVER_LIBRARY_LIB)
