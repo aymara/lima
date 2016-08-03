255e3bf29717b88b696a4ebed2de208a1f7c92cd
Sid-Ahmed Charef <sid-ahmed.charef@cea.fr>
2016-08-03 11:38:28 +0200

    reBuildRawCorpus.sh adapted to portuguese

--- lima_pelf/evalPosTagging/reBuildRawCorpus.sh
+++ lima_pelf/evalPosTagging/reBuildRawCorpus.sh
@@ -23,7 +23,7 @@ case $lang in
     eng)
         cut -f1 $file | tr '\n' ' ' | \
             sed -e "s/ \([\.\,)]\)/\\1/g" -e "s/\(['(]\) /\\1/g" -e "s/ '\(m\|re\|ll\|d\|ve\) /'\\1 /g";;
-    fre | spa | ger)
+    fre | spa | ger | por)
         cut -f1 $file | tr '\n' ' ' | \
             sed -e "s/ \([\.\,)]\)/\\1/g" -e "s/\(['(]\) /\\1/g";;
 esac
