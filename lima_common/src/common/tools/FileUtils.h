9e5c3e6c03e95ef3d2cd178edb976b566aea1cd2
Simon Marchal <simon.marchal@cea.fr>
2016-10-20 15:55:59 +0200

    Change LIMA_PATH_SEPARATOR in Qchar (Qt 4.8)
    nécessaire sinon on a une erreur c2664 : impossible de convertir de
    'char' à 'Qstring' dans 'QString::join'

--- lima_common/src/common/tools/FileUtils.h
+++ lima_common/src/common/tools/FileUtils.h
@@ -34,12 +34,7 @@
 #include <iostream>
 
 #ifdef WIN32
-#ifdef ANTINNO_SPECIFIC
-// nécessaire sinon on a une erreur c2664 : impossible de convertir de 'char' à 'Qstring' dans 'QString::join'
 static QChar const LIMA_PATH_SEPARATOR(';');
-#else
-#define LIMA_PATH_SEPARATOR ';'
-#endif
 #else
 #define LIMA_PATH_SEPARATOR ':'
 #endif
