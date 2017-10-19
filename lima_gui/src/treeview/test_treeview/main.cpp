92f5036687d1e76b74fe60103f981356f524c065
Gael de Chalendar <gael.de-chalendar@cea.fr>
2017-10-19 14:43:37 +0200

    Remove Qt 5.6 specific instruction

--- lima_gui/src/treeview/test_treeview/main.cpp
+++ lima_gui/src/treeview/test_treeview/main.cpp
@@ -28,7 +28,7 @@
 
 int main(int argc, char *argv[])
 {
-  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+//   QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
   QGuiApplication app(argc, argv);
 
   QQmlApplicationEngine engine;
