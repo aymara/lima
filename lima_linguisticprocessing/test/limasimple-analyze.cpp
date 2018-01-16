4b9ac1c4ecef027346826184c6ed209f2556ada7
Romaric Besançon <romaric.besancon@cea.fr>
2018-01-16 11:01:38 +0100

    added interactive mode to test reinit of lima client after resource change

--- lima_linguisticprocessing/test/limasimple-analyze.cpp
+++ lima_linguisticprocessing/test/limasimple-analyze.cpp
@@ -10,6 +10,8 @@
 #include "linguisticProcessing/limaSimpleClient.h"
 #include "common/Data/strwstrtools.h"
 #include <boost/program_options.hpp>
+#include <boost/algorithm/string.hpp>
+#include <iostream>
 #include <fstream>
 
 using namespace std;
@@ -25,12 +27,14 @@ struct Param {
   vector<string> inputFiles;
   string language;
   string pipeline;
+  bool interactive;
 }
 
 param={
   vector<string>(),
   "eng",
-  ""
+  "",
+  false
 };
 
 void readCommandLineArguments(unsigned int argc, char *argv[])
@@ -39,6 +43,7 @@ void readCommandLineArguments(unsigned int argc, char *argv[])
     
   desc.add_options()
     ("help,h", "Display this help message")
+    ("interactive", po::bool_switch(&(param.interactive)), "")
     ("language", po::value<std::string>(&(param.language)), "language of the documents")
     ("pipeline", po::value<std::string>(&(param.pipeline)), "pipeline to use")
     ;
@@ -62,16 +67,23 @@ void readCommandLineArguments(unsigned int argc, char *argv[])
   }
 
   // mandatory options
-  if (!vm.count("input-files") || !vm.count("language") || !vm.count("pipeline")) {
+  if (!vm.count("interactive") && (!vm.count("input-files") || !vm.count("language") || !vm.count("pipeline"))) {
     cerr << USAGE << endl; 
     exit(EXIT_FAILURE);
   }
 
 }
 
+// run analysis in interactive commmand-line mode (for debug and test reload of resources)
+void runInteractive();
+
 int main(int argc, char **argv)
 {
   readCommandLineArguments(argc,argv);
+  if (param.interactive) {
+    runInteractive();
+    return EXIT_SUCCESS;
+  }
   
   Lima::LimaSimpleClient lima;
 
@@ -86,7 +98,53 @@ int main(int argc, char **argv)
     std::cout << lima.analyze(content);
   }
   
+  //cout << "--second LIMA client" << endl;
+  // test instantiating a second LIMA client
+//   Lima::LimaSimpleClient lima2;
+//   lima2.initialize(param.language,param.pipeline);
+//   for (const auto& filename: param.inputFiles) {
+//     std::ifstream f(filename.c_str());
+//     std::string content;
+//     Lima::Common::Misc::readStream(f,content);
+//     std::cout << lima2.analyze(content);
+//   }
+
+  
 }
 
-
-
+void runInteractive()
+{
+  Lima::LimaSimpleClient lima;
+  bool quit(false);
+  cout << "limasimple-analyze interactive mode (to test reinit after resource change)" << endl
+  << "Enter a command. Possible commands are " << endl
+  << "  - init <language> <pipeline>" << endl
+  << "  - analyze <text>" << endl
+  << endl;
+  while (!quit) {
+    cout << ">";
+    string line;
+    getline(cin,line);
+    string::size_type s=line.find(" ");
+    string command(line,0,s);
+    if (command=="quit" or command.empty()) {
+      cout << endl;
+      quit=true;
+    }
+    if (command=="init") {
+      vector<string> args;
+      boost::split(args,line,boost::is_any_of(" "));
+      if (args.size()<2) {
+        cerr << "init command needs two arguments (language and pipeline)" << endl;
+      }
+      lima.initialize(args[1],args[2]);
+    }
+    else if (command=="analyze") {
+      string text(line,s+1);
+      if (text.empty()) {
+        cerr << "init command needs two arguments (language and pipeline)" << endl;
+      }
+      cout << lima.analyze(text);
+    }
+  }
+}
