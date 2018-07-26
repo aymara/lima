2ae5952aa69e183160c594db2d834bfc6c46e320
Gael de Chalendar <gael.de-chalendar@cea.fr>
2018-07-26 12:05:39 +0200

    Push docker image to the aymara organization
    Avoid to push the image to kleag personal repository

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker tag travis-ubuntu16.04 kleag/lima
-docker push kleag/lima
+docker tag travis-ubuntu16.04 aymara/lima
+docker push aymara/lima
