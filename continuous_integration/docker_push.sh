4b142c28ae82a06db8473159510cb4228d2bb15a
Gael de Chalendar <gael.de-chalendar@cea.fr>
2018-08-22 10:30:31 +0200

    Add bash guard options to avoid nasty bugs

--- continuous_integration/docker_push.sh
+++ continuous_integration/docker_push.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker tag travis-ubuntu16.04 aymara/lima
 docker push aymara/lima
