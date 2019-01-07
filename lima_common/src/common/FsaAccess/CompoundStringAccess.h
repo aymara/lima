dc257a54b6bd536eb85a06c7329d91e3cb8f8c50
Victor Bocharov <victorbocharov@users.noreply.github.com>
2019-01-07 18:54:40 +0100

    Fix logger visibility in release

--- lima_common/src/common/FsaAccess/CompoundStringAccess.h
+++ lima_common/src/common/FsaAccess/CompoundStringAccess.h
@@ -1,5 +1,5 @@
 /*
-    Copyright 2002-2013 CEA LIST
+    Copyright 2002-2019 CEA LIST
 
     This file is part of LIMA.
 
@@ -63,10 +63,10 @@ template <typename SimpleAccessByString> class CompoundStringAccess
   public:
     CompoundStringAccess(bool trie_direction_fwd=true);
     virtual ~CompoundStringAccess();
-     
+
     uint64_t getSize() const;
     int64_t getIndex( const std::vector<Lima::LimaString > & compounds ) const;
-    void getSpelling(const uint64_t index, 
+    void getSpelling(const uint64_t index,
       std::vector<Lima::LimaString > & compounds ) const;
     void read ( const std::string & filename );
     void read ( std::istream & is );
@@ -80,16 +80,16 @@ template <typename SimpleAccessByString> class CompoundStringAccess
 
     void addWord ( const std::vector<Lima::LimaString > & compounds );
     void addWord ( Lima::LimaString & word );
-    
+
   private:
     int64_t findId3( uint64_t tId1, uint64_t tId2 ) const;
     const CompoundsComposition& insertComposition( uint64_t tId1, uint64_t tId2 );
     int64_t findPos( uint64_t tId1, uint64_t tId2, uint64_t& pos ) const;
 
 //    uint64_t m_maxSimpleId;
-    
+
     SimpleAccessByString m_simpleAccessByString;
-    
+
     // utilisé par le mode builder pour gérer les mots composés (addWord)
     typedef std::map<std::pair<uint64_t,uint64_t>, uint64_t, ltTermIdPair> BuilderCompoundsSet;
     BuilderCompoundsSet m_builderCompoundsSet;
@@ -97,7 +97,7 @@ template <typename SimpleAccessByString> class CompoundStringAccess
     // utilisé par le mode reader pour gérer les mots composés
     typedef std::vector<CompoundsComposition> ReaderCompoundsSet;
     ReaderCompoundsSet m_readerCompoundsSet;
-    // utilisé par le mode reader pour la fonction getSpelling    
+    // utilisé par le mode reader pour la fonction getSpelling
     std::vector<uint64_t> m_compoundsDecomposition;
 };
 
@@ -115,16 +115,16 @@ template <typename SimpleAccessByString>
 CompoundStringAccess<SimpleAccessByString>::~CompoundStringAccess()
 {
 }
-    
+
 template <typename SimpleAccessByString>
 uint64_t CompoundStringAccess<SimpleAccessByString>::getSize() const {
   return( m_simpleAccessByString.getSize() + m_readerCompoundsSet.size() );
 }
-    
+
 template <typename SimpleAccessByString>
 int64_t CompoundStringAccess<SimpleAccessByString>::getIndex (
   const std::vector<Lima::LimaString > & compounds ) const {
-  
+
 #ifdef DEBUG_CD
   COMPSTRACCESSLOGINIT;
   LDEBUG <<  "CompoundStringAccess::getIndex(";
@@ -198,7 +198,7 @@ void CompoundStringAccess<SimpleAccessByString>::addWord (
   }
   LDEBUG << ")";
 #endif
-  
+
   // Iterateur pour parcourir du dernier au premier les composants
   std::vector<Lima::LimaString >::const_reverse_iterator compoundsIt =
     compounds.rbegin();
@@ -258,16 +258,16 @@ const CompoundsComposition& CompoundStringAccess<SimpleAccessByString>::insertCo
   COMPSTRACCESSLOGINIT;
   LDEBUG <<  "CompoundStringAccess::insertComposition(" << tId1 << "," << tId2 << ")";
 #endif
-  
+
   uint64_t pos;
   uint64_t tId3 = findPos( tId1, tId2, pos );
 #ifdef DEBUG_CD
   LDEBUG <<  "CompoundStringAccess::insertComposition: tId3=" << tId3
          << ", pos = " << pos << ")";
 #endif
-  
+
   m_compoundsDecomposition.size();
-  
+
   m_compoundsDecomposition.push_back(pos);
   CompoundsComposition composition;
   composition.cmp1Id = tId1;
@@ -286,7 +286,7 @@ int64_t CompoundStringAccess<SimpleAccessByString>::findPos(
   COMPSTRACCESSLOGINIT;
   LDEBUG <<  "CompoundStringAccess::findPos(" << tId1 << "," << tId2 << ")";
 #endif
-  
+
   uint64_t min = 0;
   uint64_t max = m_readerCompoundsSet.size();
   // test au milieu du segment [min,max[
@@ -365,7 +365,7 @@ int64_t CompoundStringAccess<SimpleAccessByString>::findPos(
 }
 
 template <typename SimpleAccessByString>
-  void CompoundStringAccess<SimpleAccessByString>::getSpelling(uint64_t index, 
+  void CompoundStringAccess<SimpleAccessByString>::getSpelling(uint64_t index,
     std::vector<Lima::LimaString > & compound ) const {
 #ifdef DEBUG_CD
   COMPSTRACCESSLOGINIT;
@@ -374,7 +374,7 @@ template <typename SimpleAccessByString>
   while( index >= MAX_SIMPLE_TID ) {
     if( (index - MAX_SIMPLE_TID) >= m_compoundsDecomposition.size() ) {
       std::ostringstream ostr;
-      ostr << "ERROR! CompoundStringAccess::getSpelling: index (" << index 
+      ostr << "ERROR! CompoundStringAccess::getSpelling: index (" << index
            << ") - MAX_SIMPLE_TID (" << MAX_SIMPLE_TID
            << ") >= m_compoundsDecomposition.size() (" << m_compoundsDecomposition.size() << ")";
       throw( AccessByStringOutOfRange(ostr.str()) );
@@ -396,7 +396,7 @@ template <typename SimpleAccessByString>
   }
   if( index >= m_simpleAccessByString.getSize() ) {
     std::ostringstream ostr;
-    ostr << "ERROR! CompoundStringAccess::getSpelling: index (" << index 
+    ostr << "ERROR! CompoundStringAccess::getSpelling: index (" << index
          << ") >= m_simpleAccessByString.getSize() (" << m_simpleAccessByString.getSize() << ")";
     throw( AccessByStringOutOfRange(ostr.str()) );
   }
@@ -404,7 +404,7 @@ template <typename SimpleAccessByString>
 }
 
 template <typename SimpleAccessByString>
-void CompoundStringAccess<SimpleAccessByString>::readSimple ( 
+void CompoundStringAccess<SimpleAccessByString>::readSimple (
   const std::string & filename )
 {
 #ifdef DEBUG_CD
@@ -412,11 +412,11 @@ void CompoundStringAccess<SimpleAccessByString>::readSimple (
   LDEBUG <<  "CompoundStringAccess::readSimple(" << filename << ")";
 #endif
   m_simpleAccessByString.read(filename);
-  
+
 }
 
 template <typename SimpleAccessByString>
-void CompoundStringAccess<SimpleAccessByString>::read ( 
+void CompoundStringAccess<SimpleAccessByString>::read (
   const std::string & filename )
 {
 #ifdef DEBUG_CD
@@ -431,42 +431,45 @@ void CompoundStringAccess<SimpleAccessByString>::read (
 #endif
     throw( AccessByStringNotInitialized( mess ) );
   }
-  
+
   CompoundStringAccess<SimpleAccessByString>::read ( is );
 }
 
 template <typename SimpleAccessByString>
-void CompoundStringAccess<SimpleAccessByString>::read ( 
+void CompoundStringAccess<SimpleAccessByString>::read (
   std::istream& is )
 {
 #ifdef DEBUG_CD
   COMPSTRACCESSLOGINIT;
   LDEBUG <<  "CompoundStringAccess::read()";
 #endif
-  
+
   m_simpleAccessByString.read( is );
-  
+
   readCompoundBody( is );
 }
 
 template <typename SimpleAccessByString>
-void CompoundStringAccess<SimpleAccessByString>::readCompoundBody( 
+void CompoundStringAccess<SimpleAccessByString>::readCompoundBody(
   std::istream& is )
 {
+#ifdef DEBUG_CD
   COMPSTRACCESSLOGINIT;
+#endif
+
 /*  uint64_t compoundsDataStreampos = m_simpleAccessByString.FsaAccessHeader::getStreamPos();
 #ifdef DEBUG_CD
   LDEBUG <<  "CompoundStringAccess::readCompoundBody(" << filename << "):"
          <<  " compoundsDataStreampos=" << compoundsDataStreampos;
 #endif
-*/  
+*/
   // offset pour atteindre les mots compos
 //  uint64_t compoundsDataOffset =
 //     m_simpleAccessByString.FsaAccessHeader::getCompoundsDataOffset();
   // nombre de mots composes pour reserver la memoire
 //  uint64_t nbCompounds =
 //     m_simpleAccessByString.FsaAccessHeader::getNbCompounds();
-  
+
 /*
   std::ifstream is(filename.c_str(), std::ios::binary );
   if( is.bad() ) {
@@ -474,15 +477,15 @@ void CompoundStringAccess<SimpleAccessByString>::readCompoundBody(
     LERROR << mess;
     throw( AccessByStringNotInitialized( mess ) );
   }
-  
-  // positionnement du fichier sur la zone de donnees des mots composes 
+
+  // positionnement du fichier sur la zone de donnees des mots composes
 #ifdef DEBUG_CD
   LDEBUG <<  "CompoundStringAccess::readCompoundBody: seek to "
          << compoundsDataStreampos;
 #endif
   is.seekg(compoundsDataStreampos,std::ios::beg) ;
 */
-  
+
   //lecture du nombre de mots composés
   uint64_t nbCompounds;
   is.read( (char *)(&(nbCompounds )), sizeof(nbCompounds ) );
@@ -490,7 +493,7 @@ void CompoundStringAccess<SimpleAccessByString>::readCompoundBody(
   LDEBUG <<  "CompoundStringAccess::readCompoundBody: nbCompounds="
          << nbCompounds;
 #endif
-    
+
   // reservation memoire
 #ifdef DEBUG_CD
   LDEBUG <<  "CompoundStringAccess::readCompoundBody: reserve memory for m_readerCompoundsSet "
@@ -502,7 +505,7 @@ void CompoundStringAccess<SimpleAccessByString>::readCompoundBody(
          << nbCompounds;
 #endif
   m_compoundsDecomposition.resize(nbCompounds);
-  
+
   // lecture de la composition des mots compose
 //  uint64_t size = m_simpleAccessByString.getSize();
   for( uint64_t n = 0 ; n < nbCompounds ; n++ ) {
@@ -522,14 +525,14 @@ void CompoundStringAccess<SimpleAccessByString>::readCompoundBody(
          << n;
 #endif
     m_compoundsDecomposition[newElement.tId3-MAX_SIMPLE_TID] = n;
-  }  
-  
+  }
+
 }
 
 template <typename SimpleAccessByString>
 void CompoundStringAccess<SimpleAccessByString>::write( const std::string & filename  )
 {
-  
+
   std::ofstream os(filename.c_str(), std::ios::out | std::ios::binary | std::ios::app );
   if( os.bad() ) {
     std::string mess = "CompoundStringAccess::write: Can't open file " + filename;
@@ -541,12 +544,12 @@ void CompoundStringAccess<SimpleAccessByString>::write( const std::string & file
   }
 //  os.seekp(HEADER_SIZE ,std::ios_base::beg );
   CompoundStringAccess<SimpleAccessByString>::write( os );
-  
+
 }
 
 template <typename SimpleAccessByString>
 void CompoundStringAccess<SimpleAccessByString>::write( std::ostream& os ) {
-  
+
   //m_simpleAccessByString.write( os );
   writeCompoundBody( os );
 }
@@ -554,18 +557,18 @@ void CompoundStringAccess<SimpleAccessByString>::write( std::ostream& os ) {
 template <typename SimpleAccessByString>
 void CompoundStringAccess<SimpleAccessByString>::writeCompoundBody(
    std::ostream& os ) {
-  
-  COMPSTRACCESSLOGINIT;
+
 #ifdef DEBUG_CD
+  COMPSTRACCESSLOGINIT;
   LDEBUG <<  "CompoundStringAccess::writeCompoundBody()";
 #endif
-  
+
   uint64_t nbCompounds = m_builderCompoundsSet.size();
 #ifdef DEBUG_CD
   LDEBUG <<  "CompoundStringAccess::writeCompoundBody: write nbCompounds = " << nbCompounds;
 #endif
   os.write( (const char*)(&nbCompounds), sizeof(nbCompounds) );
-  
+
   // sauvegarde de la composition des mots compose
   BuilderCompoundsSet::const_iterator compIt = m_builderCompoundsSet.begin();
   for( ; compIt != m_builderCompoundsSet.end() ; compIt++ ) {
@@ -581,7 +584,7 @@ void CompoundStringAccess<SimpleAccessByString>::writeCompoundBody(
     os.write( (const char*)(&cmp1Id), sizeof(cmp1Id) );
     os.write( (const char*)(&cmp2Id), sizeof(cmp2Id) );
     os.write( (const char*)(&tId3), sizeof(tId3) );
-  }  
+  }
 }
 
 
