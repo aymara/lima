6f32ea12ab1ed6d3a95a7347889dfca06431f815
Gael de Chalendar <gael.de-chalendar@cea.fr>
2019-06-19 15:29:08 +0200

    Add virtual destructor to base class

--- lima_linguisticprocessing/src/linguisticProcessing/core/TensorFlowTokenizer/DeepTokenizerBase.h
+++ lima_linguisticprocessing/src/linguisticProcessing/core/TensorFlowTokenizer/DeepTokenizerBase.h
@@ -33,6 +33,11 @@ namespace TensorFlowTokenizer
 class DeepTokenizerBase
 {
 public:
+  DeepTokenizerBase() {}
+  virtual ~DeepTokenizerBase() {}
+  DeepTokenizerBase(const DeepTokenizerBase&) {}
+  DeepTokenizerBase& operator=(const DeepTokenizerBase&) {return *this;}
+
   virtual void computeDefaultStatus(LinguisticAnalysisStructure::TStatus& curSettings);
   virtual void computeDefaultStatus(LinguisticAnalysisStructure::Token& token);
 };
