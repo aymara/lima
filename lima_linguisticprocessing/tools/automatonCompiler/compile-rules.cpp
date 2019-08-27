8c4bf3b251b85b8875e0d4ac6d40ef3088b7e6e3
Victor Bocharov <victorbocharov@users.noreply.github.com>
2019-08-27 16:02:45 +0200

    print command line arguments in compile-rules #2

--- lima_linguisticprocessing/tools/automatonCompiler/compile-rules.cpp
+++ lima_linguisticprocessing/tools/automatonCompiler/compile-rules.cpp
@@ -147,6 +147,9 @@ void readCommandLineArguments(uint64_t argc, char *argv[])
 //   bool languageSpecified(false);
   for(uint64_t i(1); i<argc; i++)
   {
+    for (size_t j = 0; j < argc; j++)
+      std::cout << "arg (" << i << ") #" << j << " == " << argv[j] << std::endl;
+
     std::string s(argv[i]);
     if (s=="-h" || s=="--help")
     {
@@ -257,8 +260,8 @@ int run(int aargc, char** aargv);
 
 int main(int argc, char **argv)
 {
-  for (size_t i = 0; i < argc; i++)
-      std::cout << "arg #" << i << " == " << argv[i] << std::endl;
+  for (size_t j = 0; j < argc; j++)
+      std::cout << "arg #" << j << " == " << argv[j] << std::endl;
   QCoreApplication a(argc, argv);
 
   // Task parented to the application so that it
