70c7027fa3e5fd3256ff59b6750a4f16258b9fa3
Victor Bocharov <victorbocharov@users.noreply.github.com>
2019-08-27 18:12:08 +0200

    Redirect LERROR to stderr

--- lima_common/src/common/AbstractFactoryPattern/AmosePluginsManager.cpp
+++ lima_common/src/common/AbstractFactoryPattern/AmosePluginsManager.cpp
@@ -62,14 +62,14 @@ bool AmosePluginsManager::loadPlugins(const QString& configDirs)
     QStringList pluginsFiles = pluginsDir.entryList(QDir::Files);
     Q_FOREACH(QString pluginsFile, pluginsFiles)
     {
-     LDEBUG << "AmosePluginsManager::loadPlugins loading plugins file "
-            << pluginsDir.path()+"/"+pluginsFile.toUtf8().data();
+      std::cerr << "AmosePluginsManager::loadPlugins loading plugins file "
+            << (pluginsDir.path()+"/"+pluginsFile).toUtf8().data() << std::endl;
       // Open plugin file.
       QFile file(pluginsDir.path() + "/" + pluginsFile);
       if (!file.open(QIODevice::ReadOnly)) {
         ABSTRACTFACTORYPATTERNLOGINIT;
-        LERROR << "AmosePluginsManager::loadPlugins: cannot open plugins file "
-                << pluginsFile.toUtf8().data();
+        std::cerr << "AmosePluginsManager::loadPlugins: cannot open plugins file "
+                << pluginsFile.toUtf8().data() << std::endl;
         return false;
       }
 
@@ -85,7 +85,7 @@ bool AmosePluginsManager::loadPlugins(const QString& configDirs)
           LDEBUG << "AmosePluginsManager::loadPlugins loading plugin '" << line.toStdString().c_str() << "'";
           if (!DynamicLibrariesManager::changeable().loadLibrary(line.toStdString().c_str()))
           {
-            LERROR << "AmosePluginsManager::loadLibrary(\"" << line.toStdString() << "\") failed.";
+            std::cerr << "AmosePluginsManager::loadLibrary(\"" << line.toStdString() << "\") failed." << std::endl;
             return false;
           }
         }
