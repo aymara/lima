7dda1d36f8f98b48ee45d4a2c8160c28eb7fe599
Victor Bocharov <victorbocharov@users.noreply.github.com>
2021-02-05 15:09:10 +0100

    Fix .bat syntax

--- continuous_integration/appveyor_win_after_build.bat
+++ continuous_integration/appveyor_win_after_build.bat
@@ -1,11 +1,11 @@
 echo %1
 cmake --build . --config Release --target install
 cmake --build . --config Release --target package
-dir C:/d/share/apps/lima/tests
-pushd C:/d/share/apps/lima/tests
-set Path = "c:/d/bin;%Path%"
-set LIMA_CONF = "C:/d/share/config/lima"
-set LIMA_RESOURCES = "C:/d/share/apps/lima/resources"
+dir C:\d\share\apps\lima\tests
+pushd C:\d\share\apps\lima\tests
+set Path = "c:\\d\\bin;%Path%"
+set LIMA_CONF = "C:\\d\\share\\config\\lima"
+set LIMA_RESOURCES = "C:\\d\\share\\apps\\lima\\resources"
 tva --language=eng test-eng.tokenizer.xml test-eng.simpleword.xml test-eng.idiom.xml test-eng.abbrev.xml test-eng.hyphen.xml test-eng.default.xml test-eng.se-PERSON.xml test-eng.se-DATE.xml test-eng.se-EMBEDED.xml test-eng.se-ORGANISATION.xml test-eng.se.xml test-eng.se-LOCATION.xml test-eng.disambiguated.xml test-eng.sa.xml 2>&1 | tee tva-eng.log
 cat tva-eng.log
 tva --language=fre test-fre.tokenizer.xml test-fre.simpleword.xml test-fre.hyphen.xml test-fre.idiom.xml test-fre.default.xml test-fre.se.xml test-fre.disambiguated.xml test-fre.sa.xml 2>&1 | tee tva-fre.log
