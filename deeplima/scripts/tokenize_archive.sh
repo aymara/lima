056b6a296e0dd1035237321c860d4cde093f58af
Victor Bocharov <victorbocharov@users.noreply.github.com>
2021-07-25 10:11:29 +0200

    Run tokenization script with 4 threads

--- deeplima/scripts/tokenize_archive.sh
+++ deeplima/scripts/tokenize_archive.sh
@@ -5,4 +5,4 @@ OUTPUT_DIR=$2
 TOK_MODEL=$3
 LNG=$4
 
-nice xzcat $INPUT_FILE | nice deeplima-eigen --tok-model=$TOK_MODEL --output-format=horizontal > $OUTPUT_DIR/${LNG}.tok.txt
+xzcat $INPUT_FILE | deeplima-eigen --tok-model=$TOK_MODEL --thread 4 --output-format=horizontal > $OUTPUT_DIR/${LNG}.tok.txt
