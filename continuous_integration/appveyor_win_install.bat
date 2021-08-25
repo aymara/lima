81e4447c8cc628369edbe201b666f334d85a5cdd
Victor Bocharov <victorbocharov@users.noreply.github.com>
2021-08-25 17:10:07 +0200

    CI (Windows): install libtorch

--- continuous_integration/appveyor_win_install.bat
+++ continuous_integration/appveyor_win_install.bat
@@ -19,10 +19,12 @@ appveyor DownloadFile https://github.com/ninja-build/ninja/releases/download/v1.
 set PATH=C:\externals\ninja;%PATH%
 ninja --version
 
+appveyor DownloadFile https://download.pytorch.org/libtorch/cpu/libtorch-win-shared-with-deps-debug-1.9.0%2Bcpu.zip libtorch.zip
+7z x libtorch.zip -oC:\external\libtorch > nul
+
 rem #ps: (new-object net.webclient).DownloadFile('http://www.nltk.org/nltk_data/packages/corpora/dependency_treebank.zip', 'c:\dependency_treebank.zip')
 mkdir c:\dependency_treebank
 unzip c:\dependency_treebank.zip -d c:\
 cd c:\
 c:\msys64\usr\bin\cat /c/dependency_treebank/wsj_*.dp | c:\msys64\usr\bin\grep -v "^$" > c:\dependency_treebank\nltk-ptb.dp
-
 
