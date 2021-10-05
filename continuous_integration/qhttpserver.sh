e0c7218e5cc2836457c56b7d3d54b768492d8994
Gael de Chalendar <gael.de-chalendar@cea.fr>
2021-10-05 15:19:41 +0200

    Allow to build without github token

--- continuous_integration/qhttpserver.sh
+++ continuous_integration/qhttpserver.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
+GITHUB_TOKEN="$1"
+
 set -o errexit
 set -o pipefail
 set -o nounset
 
-GITHUB_TOKEN=$1
 
 echo "Github API rate status: $(curl -i  https://api.github.com/repos/aymara/qhttpserver/releases/latest?access_token=$GITHUB_TOKEN) "
 
-QHTTPSERVER_LATEST_RELEASE_JSON=$(curl -s https://api.github.com/repos/aymara/qhttpserver/releases/latest?access_token=$GITHUB_TOKEN) 
-echo "QHTTPSERVER_LATEST_RELEASE_JSON=$QHTTPSERVER_LATEST_RELEASE_JSON" 
+QHTTPSERVER_LATEST_RELEASE_JSON=$(curl -s https://api.github.com/repos/aymara/qhttpserver/releases/latest?access_token=$GITHUB_TOKEN)
+echo "QHTTPSERVER_LATEST_RELEASE_JSON=$QHTTPSERVER_LATEST_RELEASE_JSON"
 
-URL=$(echo "$QHTTPSERVER_LATEST_RELEASE_JSON"| grep browser_download_url | grep debian9 | grep '.deb"' | head -n 1 | cut -d '"' -f 4) 
-echo "URL=$URL" 
-TEMP_DEB="$(mktemp)" 
-curl  -vLJ -H 'Accept: application/octet-stream' $URL?access_token=$GITHUB_TOKEN -o "$TEMP_DEB" 
+URL=$(echo "$QHTTPSERVER_LATEST_RELEASE_JSON"| grep browser_download_url | grep debian9 | grep '.deb"' | head -n 1 | cut -d '"' -f 4)
+echo "URL=$URL"
+TEMP_DEB="$(mktemp)"
+curl  -vLJ -H 'Accept: application/octet-stream' $URL?access_token=$GITHUB_TOKEN -o "$TEMP_DEB"
 dpkg -i "$TEMP_DEB"
 
