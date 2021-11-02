5cd681a27fbefc2ed45b0ed1ae37206245494b34
Gael de Chalendar <gael.de-chalendar@cea.fr>
2021-11-02 23:31:30 +0100

    Switch from unix_ar to arpy

--- lima_linguisticprocessing/src/linguisticProcessing/core/TensorFlowUnits/scripts/lima_models.py
+++ lima_linguisticprocessing/src/linguisticProcessing/core/TensorFlowUnits/scripts/lima_models.py
@@ -6,7 +6,7 @@ import os
 import re
 import argparse
 import tempfile
-import unix_ar
+import arpy
 import tarfile
 import requests
 import urllib.request
@@ -84,46 +84,46 @@ def info():
 
 
 def install_model(dir, fn, code, prefix_list):
-    ar_file = unix_ar.open(fn)
-    tarball = ar_file.open('data.tar.gz')
-    tar_file = tarfile.open(fileobj=tarball)
-    members = tar_file.getmembers()
-    for m in members:
-        if m.size > 0:
-            file = tar_file.extractfile(m)
-            if file is not None:
-                full_dir, name = os.path.split(m.name)
-                if len(prefix_list) > 0:
-                    name_prefix, _ = name.split('-')
-                    if name_prefix not in prefix_list:
-                        continue
-                mo = re.match(
-                  r'./usr/share/apps/lima/resources/(TensorFlow[A-Za-z\/\-\.0-9]+)',
-                  full_dir)
-                if mo:
-                    subdir = mo.group(1)
-                    if subdir is None or len(subdir) == 0:
-                        print(f'Error: can\'t parse \'{full_dir}\'\n',
-                              file=sys.stderr)
-                        sys.exit(1)
-                    target_dir = os.path.join(dir, subdir)
-                    os.makedirs(target_dir, exist_ok=True)
-                    with open(os.path.join(target_dir, name), 'wb') as f:
-                        while True:
-                            chunk = file.read(4096)
-                            if len(chunk) == 0:
-                                break
-                            f.write(chunk)
-                    # LIMA historically uses 'fre' for French.
-                    # This workaround adds symlinks 'fre' -> 'fra' to support
-                    # this.
-                    if code in ['fra']:
-                        src_name = os.path.join(target_dir, name)
-                        symlink_name = re.sub(r'-fra.(conf|model|bin)$',
-                                              r'-fre.\1', name, 1)
-                        symlink_name = os.path.join(target_dir, symlink_name)
-                        if not os.path.isfile(symlink_name):
-                            os.symlink(src_name, symlink_name)
+    with arpy.Archive(fn) as ar_file:
+        with ar_file.open(b'data.tar.gz') as tarball:
+            with tarfile.open(fileobj=tarball) as tar_file:
+                members = tar_file.getmembers()
+                for m in members:
+                    if m.size > 0:
+                        file = tar_file.extractfile(m)
+                        if file is not None:
+                            full_dir, name = os.path.split(m.name)
+                            if len(prefix_list) > 0:
+                                name_prefix, _ = name.split('-')
+                                if name_prefix not in prefix_list:
+                                    continue
+                            mo = re.match(
+                              r'./usr/share/apps/lima/resources/(TensorFlow[A-Za-z\/\-\.0-9]+)',
+                              full_dir)
+                            if mo:
+                                subdir = mo.group(1)
+                                if subdir is None or len(subdir) == 0:
+                                    print(f'Error: can\'t parse \'{full_dir}\'\n',
+                                          file=sys.stderr)
+                                    sys.exit(1)
+                                target_dir = os.path.join(dir, subdir)
+                                os.makedirs(target_dir, exist_ok=True)
+                                with open(os.path.join(target_dir, name), 'wb') as f:
+                                    while True:
+                                        chunk = file.read(4096)
+                                        if len(chunk) == 0:
+                                            break
+                                        f.write(chunk)
+                                # LIMA historically uses 'fre' for French.
+                                # This workaround adds symlinks 'fre' -> 'fra' to support
+                                # this.
+                                if code in ['fra']:
+                                    src_name = os.path.join(target_dir, name)
+                                    symlink_name = re.sub(r'-fra.(conf|model|bin)$',
+                                                          r'-fre.\1', name, 1)
+                                    symlink_name = os.path.join(target_dir, symlink_name)
+                                    if not os.path.isfile(symlink_name):
+                                        os.symlink(src_name, symlink_name)
 
 
 def download_binary_file(url, dir):
