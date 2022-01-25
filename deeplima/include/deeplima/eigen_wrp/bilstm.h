9dd4b8dda7bf4cefc2ef50da3b9893465719f134
Victor Bocharov <victorbocharov@users.noreply.github.com>
2022-01-25 22:12:37 +0100

    New eigen-based layers: linear, bilstm, lstm with beam decoder

--- deeplima/include/deeplima/eigen_wrp/bilstm.h
+++ deeplima/include/deeplima/eigen_wrp/bilstm.h
@@ -21,6 +21,7 @@
 #define DEEPLIMA_SRC_INFERENCE_EIGEN_BILSTM_H
 
 #include <iostream>
+#include <vector>
 #include <eigen3/Eigen/Dense>
 
 #include "op_base.h"
@@ -75,6 +76,186 @@ struct params_bilstm_t : public param_base_t
     out << fw << std::endl;
     out << bw << std::endl;
     return out;
+  }
+};
+
+template<class M, class V, class T>
+class Op_BiLSTM : public Op_Base
+{
+protected:
+  struct workbench_t : public Op_Base::workbench_t
+  {
+    workbench_t(uint32_t input_size,
+                uint32_t hidden_size,
+                bool precomputed_input=false)
+      : temp(M::Zero(precomputed_input ? 0 : hidden_size * 8, input_size)),
+        out(M::Zero(hidden_size * 2, input_size)),
+        zero(V::Zero(hidden_size)),
+        m_precomputed_input(precomputed_input)
+    {
+    }
+
+    virtual ~workbench_t() {}
+
+    M temp;
+    M out;
+    V fw_h, fw_c, bw_h, bw_c;
+    const V zero;
+    bool m_precomputed_input;
+  };
+
+public:
+  typedef V Vector;
+  typedef params_bilstm_t<M, V> params_t;
+
+  virtual workbench_t* create_workbench(uint32_t input_size, const param_base_t* params, bool precomputed_input=false) const
+  {
+    assert(input_size > 0);
+    assert(nullptr != params);
+    const params_bilstm_t<M, V>& layer = *static_cast<const params_t*>(params);
+
+    return new workbench_t(input_size, layer.fw.weight_ih.rows() / 4, precomputed_input);
+  }
+
+  virtual bool supports_precomputing() const
+  {
+    return true;
+  }
+
+  virtual void precompute_inputs(const param_base_t* params, const M& inputs, M& outputs, int64_t first_column)
+  {
+    assert(nullptr != params);
+    const params_bilstm_t<M, V>& layer = *static_cast<const params_t*>(params);
+    size_t hidden_size = layer.fw.weight_ih.rows() / 4;
+
+    auto output_block = outputs.block(0, first_column, outputs.rows(), inputs.cols());
+    // Top rows - forward pass
+    output_block.topRows(hidden_size * 4) = (layer.fw.weight_ih * inputs).colwise() + layer.fw.bias_ih;
+    // Bottom rows - backward pass
+    output_block.bottomRows(hidden_size * 4) = (layer.bw.weight_ih * inputs).colwise() + layer.bw.bias_ih;
+  }
+
+  virtual size_t execute(Op_Base::workbench_t* pwb,
+                         const M& input_matrix,
+                         const param_base_t* params,
+                         size_t input_begin,
+                         size_t input_end,
+                         Vector& fw_h,
+                         Vector& fw_c,
+                         Vector& bw_h,
+                         Vector& bw_c)
+  {
+    assert(nullptr != pwb);
+    assert(nullptr != params);
+    const params_bilstm_t<M, V>& layer = *static_cast<const params_t*>(params);
+
+    workbench_t* wb = static_cast<workbench_t*>(pwb);
+    M& temp = wb->temp;
+    M& output = wb->out;
+    const V& zero = wb->zero;
+
+    size_t hidden_size = layer.fw.weight_ih.rows() / 4;
+    V c = V::Zero(hidden_size);
+
+    M input = input_matrix.block(0, input_begin, input_matrix.rows(), input_end);
+    // TODO: rewrite this!!!
+
+    V s, g_u, g_o, g_if;
+
+    if (wb->m_precomputed_input)
+    {
+      // precomputed inputs
+      temp = input;
+    }
+    else
+    {
+      // Top rows - forward pass
+      temp.topLeftCorner(hidden_size * 4, input_end) = (layer.fw.weight_ih * input).colwise() + layer.fw.bias_ih;
+      // Bottom rows - backward pass
+      temp.bottomLeftCorner(hidden_size * 4, input_end) = (layer.bw.weight_ih * input).colwise() + layer.bw.bias_ih;
+    }
+
+    // Forward pass
+    s = temp.col(0).topRows(hidden_size * 4) + layer.fw.bias_hh + layer.fw.weight_hh * zero;
+    step_fw(hidden_size, 0, s, g_u, g_o, g_if, c, output);
+
+    for (size_t t = 1; t < input.cols(); t++)
+    {
+      s = temp.col(t).topRows(hidden_size * 4) + layer.fw.bias_hh + layer.fw.weight_hh * output.col(t-1).topRows(hidden_size);
+      step_fw(hidden_size, t, s, g_u, g_o, g_if, c, output);
+    }
+
+    int t = input.cols() - 1;
+    //V c_fw = c;
+    wb->fw_c = c;
+    wb->fw_h = output.col(t).topRows(hidden_size);
+    //t--;
+
+    // Backward pass
+    c = V::Zero(hidden_size);
+    s = temp.col(t).bottomRows(hidden_size * 4) + layer.bw.bias_hh + layer.bw.weight_hh * zero;
+    step_bw(hidden_size, t, s, g_u, g_o, g_if, c, output);
+
+    for (t = input.cols() - 2; t >= 0; t--)
+    {
+      s = temp.col(t).bottomRows(hidden_size * 4) + layer.bw.bias_hh + layer.bw.weight_hh * output.col(t+1).bottomRows(hidden_size);
+      step_bw(hidden_size, t, s, g_u, g_o, g_if, c, output);
+    }
+    wb->bw_c = c;
+    wb->bw_h = output.col(0).bottomRows(hidden_size);
+
+    fw_h = wb->fw_h;
+    fw_c = wb->fw_c;
+    bw_h = wb->bw_h;
+    bw_c = wb->bw_c;
+
+    return 0;
+  }
+
+protected:
+  inline void step_fw(
+        size_t hidden_size, // [in]     LSTM parameter
+        size_t t,           // [in]     step (position in output)
+        const V& s,         // [in]     result before gating
+        V& g_u,             // [temp]   update gate
+        V& g_o,             // [temp]   output gate
+        V& g_if,            // [temp]   input and forget gates
+        V& c,               // [in/out] cell state
+        M& output           // [out]    matrix of output states
+      )
+  {
+    g_if = 1 / (1 + Eigen::exp( 0 - s.segment(0, hidden_size * 2).array() ) );
+    g_u = 2 / (1 + Eigen::exp( 0 - 2 * s.segment(hidden_size * 2, hidden_size).array() ) ) - 1; // tanh
+    g_o = 1 / (1 + Eigen::exp( 0 - s.segment(hidden_size * 3, hidden_size).array() ) );
+
+    c = g_if.segment(0, hidden_size).cwiseProduct(g_u) + g_if.segment(hidden_size, hidden_size).cwiseProduct(c);
+
+    output.col(t).topRows(hidden_size) = g_o.cwiseProduct(c.unaryExpr( [](float x) { return my_tanh(x); } ));
+  }
+
+  inline void step_bw(
+        size_t hidden_size, // [in]     LSTM parameter
+        size_t t,           // [in]     step (position in output)
+        const V& s,         // [in]     result before gating
+        V& g_u,             // [temp]   update gate
+        V& g_o,             // [temp]   output gate
+        V& g_if,            // [temp]   input and forget gates
+        V& c,               // [in/out] cell state
+        M& output           // [out]    matrix of output states
+      )
+  {
+    g_if = 1 / (1 + Eigen::exp( 0 - s.segment(0, hidden_size * 2).array() ) );
+    g_u = 2 / (1 + Eigen::exp( 0 - 2 * s.segment(hidden_size * 2, hidden_size).array() ) ) - 1; // tanh
+    g_o = 1 / (1 + Eigen::exp( 0 - s.segment(hidden_size * 3, hidden_size).array() ) );
+
+    c = g_if.segment(0, hidden_size).cwiseProduct(g_u) + g_if.segment(hidden_size, hidden_size).cwiseProduct(c);
+
+    output.col(t).bottomRows(hidden_size) = g_o.cwiseProduct(c.unaryExpr( [](float x) { return my_tanh(x); } ));
+  }
+
+  inline static float my_tanh(float x)
+  {
+    return ::tanhf(x);
   }
 };
 
---
+++ deeplima/include/deeplima/eigen_wrp/lstm_beam_decoder.h
@@ -0,0 +1,366 @@
+/*
+    Copyright 2021 CEA LIST
+
+    This file is part of LIMA.
+
+    LIMA is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    LIMA is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with LIMA.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+#ifndef DEEPLIMA_SRC_INFERENCE_EIGEN_LSTM_BEAM_DECODER_H
+#define DEEPLIMA_SRC_INFERENCE_EIGEN_LSTM_BEAM_DECODER_H
+
+#include <vector>
+
+#include "bilstm.h"
+#include "linear.h"
+
+#include "embd_dict.h"
+
+namespace deeplima
+{
+namespace eigen_impl
+{
+
+template<class M, class V>
+struct params_lstm_beam_decoder_t : public param_base_t
+{
+  params_bilstm_t<M, V> bilstm;
+  std::vector<params_linear_t<M, V>> linear;
+};
+
+template<class M, class V, class T>
+class Op_LSTM_Beam_Decoder : public Op_Base
+{
+protected:
+  struct workbench_t : public Op_Base::workbench_t
+  {
+    workbench_t(uint32_t hidden_size,
+                const std::vector<uint32_t>& output_sizes,
+                bool precomputed_input=false)
+      : temp(M::Zero(precomputed_input ? 0 : hidden_size * 4, 1)),
+        out(M::Zero(hidden_size, 1)),
+        zero(V::Zero(hidden_size)),
+        m_precomputed_input(precomputed_input)
+    {
+      lin_out.resize(output_sizes.size());
+      for (size_t i = 0; i < output_sizes.size(); i++)
+      {
+        lin_out[i] = M::Zero(output_sizes[i], 1);
+      }
+    }
+
+    virtual ~workbench_t() {}
+
+    M temp;
+    M out;
+    std::vector<M> lin_out;
+    const V zero;
+    bool m_precomputed_input;
+  };
+
+  struct decoding_timepoint_t
+  {
+    size_t prev_pos;
+    uint32_t cls;
+
+    decoding_timepoint_t() : prev_pos(0), cls(0) { }
+  };
+
+public:
+
+  typedef params_lstm_beam_decoder_t<M, V> params_t;
+
+  virtual workbench_t* create_workbench(uint32_t input_size, const param_base_t* params, bool precomputed_input=false) const
+  {
+    assert(nullptr != params);
+    const params_bilstm_t<M, V>& layer = static_cast<const params_t*>(params)->bilstm;
+    const std::vector<params_linear_t<M, V>>& linear = static_cast<const params_t*>(params)->linear;
+
+    std::vector<uint32_t> output_sizes;
+    output_sizes.reserve(linear.size());
+    for ( const auto& p : linear )
+    {
+      output_sizes.push_back(p.weight.rows());
+    }
+    return new workbench_t(layer.fw.weight_ih.rows() / 4, output_sizes, precomputed_input);
+  }
+
+  virtual bool supports_precomputing() const
+  {
+    return true;
+  }
+
+  virtual void precompute_inputs(const param_base_t* params, const M& inputs, M& outputs, int64_t first_column)
+  {
+    assert(nullptr != params);
+    const params_bilstm_t<M, V>& layer = static_cast<const params_t*>(params)->bilstm;
+    size_t hidden_size = layer.fw.weight_ih.rows() / 4;
+
+    auto output_block = outputs.block(0, first_column, outputs.rows(), inputs.cols());
+    // Top rows - forward pass
+    output_block.topRows(hidden_size * 4) = (layer.fw.weight_ih * inputs).colwise() + layer.fw.bias_ih;
+    // Bottom rows - backward pass
+    output_block.bottomRows(hidden_size * 4) = (layer.bw.weight_ih * inputs).colwise() + layer.bw.bias_ih;
+  }
+
+  virtual size_t execute(Op_Base::workbench_t* pwb,
+                         const EmbdUInt64Float& embd,
+                         const V& initial_state_h,
+                         const V& initial_state_c,
+                         const param_base_t* params,
+                         uint32_t start_code,
+                         size_t beam_size,
+                         std::vector<uint32_t>& output_seq,
+                         size_t max_output_len)
+  {
+    assert(nullptr != pwb);
+    assert(nullptr != params);
+    const params_bilstm_t<M, V>& layer = static_cast<const params_t*>(params)->bilstm;
+    const params_linear_t<M, V>& linear = static_cast<const params_t*>(params)->linear[0];
+
+    workbench_t* wb = static_cast<workbench_t*>(pwb);
+    M& temp = wb->temp;
+    M& output = wb->out;
+    const V& zero = wb->zero;
+
+    std::vector<std::vector<decoding_timepoint_t>> decoding_log(max_output_len, std::vector<decoding_timepoint_t>(beam_size));
+
+    size_t hidden_size = layer.fw.weight_ih.rows() / 4;
+    V s, g_u, g_o, g_if;
+    V c = initial_state_c;
+    V h = initial_state_h;
+
+    M input(embd.dim(), 1); //= input_matrix.block(0, input_begin, input_matrix.rows(), temp.cols());
+    embd.get(start_code, input, 0, 0);
+
+    std::vector<uint32_t> initial_top_classes(beam_size);
+    std::vector<float> initial_logprob(beam_size);
+    step_with_decoding(layer, linear, input, output, s, g_u, g_o, g_if, temp, hidden_size, beam_size, 0,
+                       initial_top_classes, initial_logprob, c, h);
+
+    size_t decoding_step = 0;
+    for (size_t i = 0; i < beam_size; ++i)
+    {
+      decoding_log[decoding_step][i].cls = initial_top_classes[i];
+    }
+    decoding_step++;
+
+    M states_c(hidden_size, beam_size);
+    for (size_t i = 0; i < beam_size; ++i) states_c.col(i) = c;
+    M states_h(hidden_size, beam_size);
+    for (size_t i = 0; i < beam_size; ++i) states_h.col(i) = h;
+
+    while (decoding_step < max_output_len)
+    {
+      std::vector<uint32_t> top_classes(beam_size * beam_size);
+      std::vector<float> logprob(beam_size * beam_size);
+      for (size_t i = 0; i < beam_size; ++i)
+      {
+        V tc = states_c.col(i);
+        V th = states_h.col(i);
+        embd.get_direct(decoding_log[decoding_step - 1][i].cls, input, 0, 0);
+        step_with_decoding(layer, linear, input, output, s, g_u, g_o, g_if, temp,
+                           hidden_size, beam_size,
+                           i * beam_size,
+                           top_classes, logprob, tc, th); //, states_h.col(i));
+        states_c.col(i) = tc;
+        states_h.col(i) = th;
+        for (size_t j = 0; j < beam_size; ++j)
+        {
+          logprob[i * beam_size + j] += initial_logprob[i];
+        }
+      }
+
+      std::vector<uint32_t> indices(beam_size * beam_size);
+      for (uint32_t i = 0; i < beam_size * beam_size; ++i) indices[i] = i;
+      std::sort(indices.begin(), indices.end(), [&logprob](size_t a, size_t b){
+        return logprob[a] > logprob[b];
+      });
+
+      /*
+      for (size_t i = 0; i < beam_size * beam_size; ++i)
+      {
+        size_t idx = indices[i];
+        std::cerr << i << " " << idx << " "
+                     << initial_top_classes[idx % beam_size] << " " << top_classes[idx]
+                     << " "
+                     << initial_logprob[idx % beam_size] << " " << logprob[idx] << std::endl;
+      }
+      */
+
+      for (size_t i = 0; i < beam_size; ++i)
+      {
+        size_t idx = indices[i];
+        size_t ref = idx % beam_size;
+        states_c.col(i) = states_c.col(ref);
+        states_h.col(i) = states_h.col(ref);
+      }
+
+      for (size_t i = 0; i < beam_size; ++i)
+      {
+        size_t idx = indices[i];
+        decoding_log[decoding_step][i].cls = top_classes[idx];
+        decoding_log[decoding_step][i].prev_pos = idx % beam_size;
+        initial_logprob[i] = logprob[idx];
+      }
+
+      decoding_step++;
+
+      //std::cerr << std::endl;
+    }
+
+    /*
+    for (size_t p = 0; p < beam_size; ++p)
+    {
+      for (size_t step = 0; step < decoding_log.size(); ++step)
+      {
+        char32_t ch = embd.decode(decoding_log[step][p].cls);
+        std::cerr << decoding_log[step][p].prev_pos
+                  << ":"
+                  << decoding_log[step][p].cls
+                  << ":"
+                  << (char)ch
+                  << " ";
+
+      }
+      std::cerr << std::endl;
+    }
+
+    std::cerr << std::endl;
+    */
+
+    // Backtracking
+    int step = 0;
+    size_t pos = 0;
+    while (step < decoding_log.size() && decoding_log[step][pos].cls != 1) step++;
+    step--;
+    while (step >= 0)
+    {
+      char32_t ch = embd.decode(decoding_log[step][pos].cls);
+      output_seq.push_back(ch);
+      pos = decoding_log[step][pos].prev_pos;
+      step--;
+    }
+    std::reverse(output_seq.begin(), output_seq.end());
+
+    return 0;
+  }
+
+protected:
+
+  inline void step_with_decoding(
+        const params_bilstm_t<M, V>& layer,
+        const params_linear_t<M, V>& linear,
+        const M& input,                      // [in]
+        M& output,                           // [temp]
+        V& s,                                // [in]     result before gating
+        V& g_u,                              // [temp]   update gate
+        V& g_o,                              // [temp]   output gate
+        V& g_if,                             // [temp]   input and forget gates
+        M& temp,                             // [temp]
+        size_t hidden_size,                  // [in]
+        size_t beam_size,                    // [in]
+        size_t start_pos,                    // [in]
+        std::vector<uint32_t>& top_classes,  // [out]
+        std::vector<float>& logprob,         // [out]
+        V& c,                                // [in/out]
+        V& h                                 // [in/out]
+      )
+  {
+    // Top rows - forward pass
+    temp.topRows(hidden_size * 4) = (layer.fw.weight_ih * input).colwise() + layer.fw.bias_ih;
+
+    // Forward pass
+    s = temp.col(0).topRows(hidden_size * 4) + layer.fw.bias_hh + layer.fw.weight_hh * h;
+    step_fw(hidden_size, 0, s, g_u, g_o, g_if, c, output);
+    h = output.col(0).topRows(hidden_size);
+
+    // Linear
+    V lin_out = (linear.weight * output).colwise() + linear.bias;
+    Eigen::Index idx = 0;
+    typename V::Scalar max_value = lin_out.maxCoeff(&idx);
+    auto d1 = lin_out.array() - max_value;
+    auto d2 = Eigen::exp(d1);
+    typename V::Scalar d3 = d2.sum();
+    typename V::Scalar d4 = max_value + d3;
+    Eigen::ArrayXf all_logprob = lin_out.array() - d4;
+    std::vector<uint32_t> indices(lin_out.rows());
+    for (uint32_t i = 0; i < indices.size(); ++i) indices[i] = i;
+
+    std::partial_sort_copy(indices.begin(), indices.end(), top_classes.begin() + start_pos, top_classes.begin() + start_pos + beam_size,
+                           [&all_logprob](uint32_t a, uint32_t b) {
+      return all_logprob[a] > all_logprob[b];
+    });
+
+    for (size_t i = 0; i < beam_size; ++i)
+    {
+      logprob[start_pos + i] = all_logprob[top_classes[start_pos + i]];
+      /*
+      std::cerr << i << " " << top_classes[start_pos + i]
+                     << " " << all_logprob[top_classes[start_pos + i]]
+                     << " " << lin_out[top_classes[start_pos + i]] << std::endl;
+      */
+    }
+  }
+
+  inline void step_fw(
+        size_t hidden_size, // [in]     LSTM parameter
+        size_t t,           // [in]     step (position in output)
+        const V& s,         // [in]     result before gating
+        V& g_u,             // [temp]   update gate
+        V& g_o,             // [temp]   output gate
+        V& g_if,            // [temp]   input and forget gates
+        V& c,               // [in/out] cell state
+        M& output           // [out]    matrix of output states
+      )
+  {
+    g_if = 1 / (1 + Eigen::exp( 0 - s.segment(0, hidden_size * 2).array() ) );
+    g_u = 2 / (1 + Eigen::exp( 0 - 2 * s.segment(hidden_size * 2, hidden_size).array() ) ) - 1; // tanh
+    g_o = 1 / (1 + Eigen::exp( 0 - s.segment(hidden_size * 3, hidden_size).array() ) );
+
+    c = g_if.segment(0, hidden_size).cwiseProduct(g_u) + g_if.segment(hidden_size, hidden_size).cwiseProduct(c);
+
+    output.col(t).topRows(hidden_size) = g_o.cwiseProduct(c.unaryExpr( [](float x) { return my_tanh(x); } ));
+  }
+
+  inline void step_bw(
+        size_t hidden_size, // [in]     LSTM parameter
+        size_t t,           // [in]     step (position in output)
+        const V& s,         // [in]     result before gating
+        V& g_u,             // [temp]   update gate
+        V& g_o,             // [temp]   output gate
+        V& g_if,            // [temp]   input and forget gates
+        V& c,               // [in/out] cell state
+        M& output           // [out]    matrix of output states
+      )
+  {
+    g_if = 1 / (1 + Eigen::exp( 0 - s.segment(0, hidden_size * 2).array() ) );
+    g_u = 2 / (1 + Eigen::exp( 0 - 2 * s.segment(hidden_size * 2, hidden_size).array() ) ) - 1; // tanh
+    g_o = 1 / (1 + Eigen::exp( 0 - s.segment(hidden_size * 3, hidden_size).array() ) );
+
+    c = g_if.segment(0, hidden_size).cwiseProduct(g_u) + g_if.segment(hidden_size, hidden_size).cwiseProduct(c);
+
+    output.col(t).bottomRows(hidden_size) = g_o.cwiseProduct(c.unaryExpr( [](float x) { return my_tanh(x); } ));
+  }
+
+  inline static float my_tanh(float x)
+  {
+    return ::tanhf(x);
+  }
+};
+
+} // namespace eigen_impl
+} // namespace deeplima
+
+#endif
