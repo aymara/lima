c0bb63be932246837a53710176a63226faaccc66
deurstann <tristan.derouet@gmail.com>
2022-03-09 16:58:13 +0100

    Corrected the langDetector header to add a function to correctly print the language

--- lima_linguisticprocessing/src/linguisticProcessing/core/LangDetector/langDetector.h
+++ lima_linguisticprocessing/src/linguisticProcessing/core/LangDetector/langDetector.h
@@ -6,10 +6,17 @@
 
 using namespace std;
 
+/**
+ * @brief Main class of the language detector module.
+ * 
+ */
 class LangDetector {
     public:
         LangDetector(string modelPath);
+        /*This function takes a sentence for input and return the associated language label.*/
         const string detectLang(string sentence);
+        /*Used to convert a language label "__label__lang" to a printable format "lang".*/
+        const string labelToPrintable(string label);
     private:
         fasttext::FastText m_fasttext;
 };
