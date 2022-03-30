6b59b7c215036a37158e8c1b7bed914983889a14
Gael de Chalendar <gael.de-chalendar@cea.fr>
2022-03-30 22:46:50 +0200

    Add missing libtorch install to dockerfile

--- extern/download_libtorch.sh
+++ extern/download_libtorch.sh
@@ -1,3 +1,3 @@
 
-wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.9.0%2Bcpu.zip
-unzip libtorch-cxx11-abi-shared-with-deps-1.9.0+cpu.zip
+wget -q https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.9.0%2Bcpu.zip
+unzip -q libtorch-cxx11-abi-shared-with-deps-1.9.0+cpu.zip
