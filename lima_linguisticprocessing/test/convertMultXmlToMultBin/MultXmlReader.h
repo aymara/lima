1f7f3f26c2b76894ab60c83cfcc8c3ff378f21fc
Gael de Chalendar <gael.de-chalendar@cea.fr>
2022-06-11 08:42:22 +0200

    Correct parsing of mult xml files

--- lima_linguisticprocessing/test/convertMultXmlToMultBin/MultXmlReader.h
+++ lima_linguisticprocessing/test/convertMultXmlToMultBin/MultXmlReader.h
@@ -1,15 +1,21 @@
-/************************************************************************
- *
- * @file       MultXmlReader.h
- * @author     Gael de Chalendar (gael.de-chalendar@cea.fr)
- * @date       Mon Apr 7 2014
- * copyright   Copyright (C) 2014 by CEA LIST
- * Project     lima_xmlprocessings
- *
- * Parser for XML representation of Amose analysed multimedia documents
- *
- ***********************************************************************/
+/*
+    Copyright 2002-2022 CEA LIST
 
+    This file is part of LIMA.
+
+    LIMA is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    LIMA is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with LIMA.  If not, see <http://www.gnu.org/licenses/>
+*/
 #ifndef MULTXMLREADER_H
 #define MULTXMLREADER_H
 
@@ -22,6 +28,8 @@ namespace XmlReader {
 class MultXmlReaderPrivate;
 /**
  * @brief Parser for XML representation of Amose analysed multimedia documents
+ * @author     Gael de Chalendar (gael.de-chalendar@cea.fr)
+ * @date       Mon Apr 7 2014
  */
 class MultXmlReader
 {
