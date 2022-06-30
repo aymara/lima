2acd2214a314fb933176185ac1cd7267ae11d5a5
Romaric Besancon <romaric.besancon@cea.fr>
2022-06-30 12:46:11 +0200

    Add possibility to restrict event merging to sentence boundaries.

--- lima_linguisticprocessing/src/linguisticProcessing/core/EventAnalysis/EventTemplateMerging.h
+++ lima_linguisticprocessing/src/linguisticProcessing/core/EventAnalysis/EventTemplateMerging.h
@@ -33,6 +33,7 @@
 
 #include "EventAnalysisExport.h"
 #include "EventTemplateStructure.h"
+#include "EventTemplateData.h"
 #include "EventTemplate.h"
 #include "EventTemplateDefinitionResource.h"
 #include "common/MediaProcessors/MediaProcessUnit.h"
@@ -61,9 +62,12 @@ private:
   EventTemplateDefinitionResource* m_templateDefinition;
   std::deque<std::string> m_mandatoryElements;
   uint64_t m_maxCharCompatibleEvents;
+  bool m_useSentenceBounds;
+  std::string m_sentenceBoundsData; // name of data containing the boundaries if m_useSentenceBounds
   
   // private member function
-  LimaStatusCode mergeEventTemplates(AnalysisContent& analysis) const;
+  LimaStatusCode mergeEventTemplatesOnEachSentence(AnalysisContent& analysis, EventTemplateData* eventData) const;
+  LimaStatusCode mergeEventTemplates(EventTemplateData* eventData, uint64_t posBegin, uint64_t posEnd) const;
   bool compatibleTemplates(const EventTemplate& e1, const EventTemplate& e2,
                            unsigned int numTemplate, unsigned int numOtherTemplate,
                            std::map<std::string, std::pair<std::string, int> >& matchingElements) const;
@@ -72,7 +76,13 @@ private:
   bool mergeTemplates(EventTemplate& e1, EventTemplate& e2,
                       unsigned int numTemplate, unsigned int numOtherTemplate,
                       unsigned int& templateToRemove) const;
-
+                      
+  bool ignoreTemplate(const EventTemplate& event, uint64_t numTemplate,
+                      uint64_t posBegin, uint64_t posEnd, 
+                      const std::set<uint64_t>& toRemove) const;
+                      
+  void cleanEventTemplates(EventTemplateData* eventData) const;
+                      
   
 };
 
