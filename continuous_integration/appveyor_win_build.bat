5c6e5f3b92a19f03c75270b66f63439b7fe36993
Deurstann <tristan.derouet@gmail.com>
2022-07-20 13:54:11 +0200

    correcly installed eigen

--- continuous_integration/appveyor_win_build.bat
+++ continuous_integration/appveyor_win_build.bat
@@ -13,7 +13,7 @@ cd C:\externals\eigen-3.4.0\
 mkdir build
 cd build
 cmake ..
-make install
+cmake --build . --target install
 cd c:\b
 rmdir /S /Q C:\externals\eigen-3.4.0\
 cmake -G "Ninja" "-DCMAKE_PREFIX_PATH=%QTDIR%;c:/projects/lima/extern/libtorch/;c:/Program Files (x86)/Eigen3;c:/externals/icu4c/;c:/externals/tensorflow-1.9.0/" "-DLIMA_RESOURCES=build" "-DLIMA_VERSION_RELEASE:STRING=%LIMA_VERSION_RELEASE%" "-DCMAKE_BUILD_TYPE=RelWithDebInfo" "-DCMAKE_INSTALL_PREFIX=c:\d" "-DBoost_DEBUG=ON" "-DBoost_COMPILER=-vc142" "-DBOOST_LIBRARYDIR:STRING=%BOOST_LIBRARYDIR%" -DEigen3_DIR="C:\Program Files (x86)\Eigen3" -DCMAKE_IGNORE_PATH="C:/msys64/usr/bin" -DWITH_DEBUG_MESSAGES=ON -DWITH_ARCH=OFF -DWITH_ASAN=OFF -DSHORTEN_POR_CORPUS_FOR_SVMLEARN=ON -DWITH_GUI=ON c:/projects/lima
