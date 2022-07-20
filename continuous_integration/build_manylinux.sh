10b333d26749dfb7424622730a6eaea811c38199
Gael de Chalendar <gael.de-chalendar@cea.fr>
2022-07-20 18:15:17 +0200

    Correct github action

--- continuous_integration/build_manylinux.sh
+++ continuous_integration/build_manylinux.sh
@@ -8,5 +8,7 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-docker build -f Dockerfile-manylinux_2_24 --build-arg CACHEBUST=$(date +%s) .  -t aymara/lima-manylinux_2_24:latest
-docker push aymara/lima-manylinux_2_24:latest
+export PYTHON_VERSION=3.7
+export PYTHON_SHORT_VERSION=37
+docker build -f Dockerfile-manylinux_2_24 --build-arg PYTHON_VERSION=${PYTHON_VERSION} --build-arg PYTHON_SHORT_VERSION=${PYTHON_SHORT_VERSION} --build-arg CACHEBUST=$(date +%s) .  -t aymara/lima-manylinux_2_24:latest
+# docker push aymara/lima-manylinux_2_24-python${PYTHON_VERSION}:latest
