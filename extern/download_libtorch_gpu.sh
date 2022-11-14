8f38498a7bdd9b66520f09775b6d0ad59d8cedc1
Deurstann <tristan.derouet@gmail.com>
2022-11-14 15:31:22 +0100

    added script to download libtorch GPU

---
+++ extern/download_libtorch_gpu.sh
@@ -0,0 +1,4 @@
+
+wget -q https://download.pytorch.org/libtorch/cu113/libtorch-cxx11-abi-shared-with-deps-1.12.0%2Bcu113.zip
+unzip -q libtorch-cxx11-abi-shared-with-deps-1.12.0+cu113.zip
+rm -f libtorch-cxx11-abi-shared-with-deps-1.12.0+cu113.zip
