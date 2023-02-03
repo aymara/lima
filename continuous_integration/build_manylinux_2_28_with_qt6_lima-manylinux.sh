d62ce26fe15cf8e5ac21e1580743b7b993f26cf7
Gael de Chalendar <gael.de-chalendar@cea.fr>
2023-02-03 01:13:26 +0100

    Pass pyside version to docker build

--- continuous_integration/build_manylinux_2_28_with_qt6_lima-manylinux.sh
+++ continuous_integration/build_manylinux_2_28_with_qt6_lima-manylinux.sh
@@ -9,6 +9,7 @@ set -o pipefail
 set -o nounset
 
 source python_env_qt6.sh
+# --no-cache \
 docker build -f Dockerfile-manylinux_2_28_with_qt6_lima-manylinux \
     --build-arg MANYLINUX_TAG="${MANYLINUX_TAG}" \
     --build-arg GCC_VERSION="${GCC_VERSION}" \
@@ -19,6 +20,7 @@ docker build -f Dockerfile-manylinux_2_28_with_qt6_lima-manylinux \
     --build-arg QT_VERSION_MINOR="${QT_VERSION_MINOR}" \
     --build-arg QT_VERSION_PATCH="${QT_VERSION_PATCH}" \
     --build-arg QT_VERSION="${QT_VERSION}" \
+    --build-arg PYSIDE_VERSION=${PYSIDE_VERSION} \
     --build-arg PYTHON_VERSION=${PYTHON_VERSION} \
     --build-arg PYTHON_SHORT_VERSION=${PYTHON_SHORT_VERSION} \
     --build-arg PYTHON_FULL_VERSION=${PYTHON_FULL_VERSION} \
