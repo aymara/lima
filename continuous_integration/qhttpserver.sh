1ad2b164cadf468d1c80977489b0bf01b98a3b60
B. Labbe <38100506+benlabbe@users.noreply.github.com>
2023-10-09 17:23:57 +0200

    Update qhttpserver.sh

--- continuous_integration/qhttpserver.sh
+++ continuous_integration/qhttpserver.sh
@@ -16,7 +16,7 @@ echo "Github API rate status: $(curl -i  https://api.github.com/repos/aymara/qht
 QHTTPSERVER_LATEST_RELEASE_JSON=$(curl -s https://api.github.com/repos/aymara/qhttpserver/releases/latest?access_token=$GITHUB_TOKEN)
 echo "QHTTPSERVER_LATEST_RELEASE_JSON=$QHTTPSERVER_LATEST_RELEASE_JSON"
 
-URL=$(echo "$QHTTPSERVER_LATEST_RELEASE_JSON"| grep browser_download_url | grep debian9 | grep '.deb"' | head -n 1 | cut -d '"' -f 4)
+URL=$(echo "$QHTTPSERVER_LATEST_RELEASE_JSON"| grep browser_download_url | grep debian11 | grep '.deb"' | head -n 1 | cut -d '"' -f 4)
 echo "URL=$URL"
 TEMP_DEB="$(mktemp)"
 curl  -LJ -H 'Accept: application/octet-stream' $URL?access_token=$GITHUB_TOKEN -o "$TEMP_DEB"
