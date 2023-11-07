3d44f37de1ea03cec9714dfab3cba605ddf1b586
B. Labbe <38100506+benlabbe@users.noreply.github.com>
2023-11-08 00:46:05 +0100

    Update qhttpserver.sh - remove usage of github access_token

--- continuous_integration/qhttpserver.sh
+++ continuous_integration/qhttpserver.sh
@@ -4,21 +4,18 @@
 #
 # SPDX-License-Identifier: MIT
 
-GITHUB_TOKEN="$1"
-
 set -o errexit
 set -o pipefail
 set -o nounset
 
-
-echo "Github API rate status: $(curl -i  https://api.github.com/repos/aymara/qhttpserver/releases/latest?access_token=$GITHUB_TOKEN) "
-
-QHTTPSERVER_LATEST_RELEASE_JSON=$(curl -s https://api.github.com/repos/aymara/qhttpserver/releases/latest?access_token=$GITHUB_TOKEN)
+QHTTPSERVER_LATEST_RELEASE_JSON=$(curl -s https://api.github.com/repos/aymara/qhttpserver/releases/latest)
 echo "QHTTPSERVER_LATEST_RELEASE_JSON=$QHTTPSERVER_LATEST_RELEASE_JSON"
 
 URL=$(echo "$QHTTPSERVER_LATEST_RELEASE_JSON"| grep browser_download_url | grep debian11 | grep '.deb"' | head -n 1 | cut -d '"' -f 4)
 echo "URL=$URL"
-TEMP_DEB="$(mktemp)"
-curl  -LJ -H 'Accept: application/octet-stream' $URL?access_token=$GITHUB_TOKEN -o "$TEMP_DEB"
-dpkg -i "$TEMP_DEB"
-
+TEMP_DEB_DIR="$(mktemp -d)"
+curl  -LJ -H 'Accept: application/octet-stream' $URL --output-dir "$TEMP_DEB_DIR" --output release.tar
+pushd "$TEMP_DEB_DIR"
+tar xf ./release.tar
+dpkg -i *.deb
+popd
